d07808fb1a0cd20a21c2a73a469f45b04080b626
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-09-19 00:55:40 +0000

    updated

--- Project/test_script.py
+++ Project/test_script.py
@@ -2,6 +2,9 @@ import script
 import pandas as pd
 
 def test_run_desc_stat():
+    df = pd.read_csv('Development of Average Annual Wages.csv')
+    col = '2022'
+    
     result = script.run_desc_stat(df, col)
     assert 'Target Column' in result
     assert 'Mean' in result
@@ -9,6 +12,4 @@ def test_run_desc_stat():
     assert 'Standard Deviation' in result
 
 if __name__ == "__main__":
-    df = pd.read_csv('Development of Average Annual Wages.csv')
-    col = '2022'
     test_run_desc_stat()
